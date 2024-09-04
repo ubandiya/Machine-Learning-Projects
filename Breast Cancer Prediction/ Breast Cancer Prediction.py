61e65e45647c5147b803f6d8b89b880f1f90851f
Najib Yusuf Ubandiya <32720837+ubandiya@users.noreply.github.com>
2024-09-04 20:14:39 +0100

    Update  Breast Cancer Prediction.py

--- Breast Cancer Prediction/ Breast Cancer Prediction.py
+++ Breast Cancer Prediction/ Breast Cancer Prediction.py
@@ -16,11 +16,11 @@ data = load_breast_cancer()
 df = pd.DataFrame(data=data.data, columns=data.feature_names)
 df['target'] = data.target
 
-'''plt.figure(figsize=(20, 10))
+plt.figure(figsize=(20, 10))
 sns.boxplot(data=df.drop('target', axis=1))
 plt.xticks(rotation=90)
 plt.title('Box Plot of Features')
-plt.show()'''
+plt.show()
 
 z_scores = stats.zscore(df.drop('target', axis=1))
 abs_z_scores = np.abs(z_scores)
@@ -35,17 +35,18 @@ y = df['target'].map({0: 'No cancer', 1: 'Cancer'})
 
 selector = SelectKBest(f_classif, k=10)
 X_new = selector.fit(X, y)
-#print(f'Selected features: {X.columns[selector.get_support()]}')
+print(f'Selected features: {X.columns[selector.get_support()]}')
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test_scaled)
 
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42, stratify=y)
 knn = KNeighborsClassifier(n_neighbors=19)
-knn.fit(X_train, y_train)
+knn.fit(X_train_scaled, y_train)
 
-y_pred = knn.predict(X_test)
+y_pred = knn.predict(X_test_scaled)
 
 accuracy = accuracy_score(y_test, y_pred)
 class_ = classification_report(y_test, y_pred)
@@ -61,18 +62,18 @@ cv_scores = []
 
 for k in k_values:
     knn_ = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn_, X_train, y_train, cv=5, scoring='accuracy')
+    scores = cross_val_score(knn_, X_train_scaled, y_train, cv=5, scoring='accuracy')
     cv_scores.append(scores.mean())
 
 print(f'The best value for k: {k_values[cv_scores.index(max(cv_scores))]}')
 
-'''sns.heatmap(conf_, annot=True, fmt='d', cmap='Blues', xticklabels=['No cancer', 'Cancer'], yticklabels=['No cancer', 'Cancer'])
+sns.heatmap(conf_, annot=True, fmt='d', cmap='Blues', xticklabels=['No cancer', 'Cancer'], yticklabels=['No cancer', 'Cancer'])
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.title('Cofusion Matrix')
-plt.show()'''
+plt.show()
 
-synth_data = np.random.normal(loc=np.mean(X_scaled, axis=0), scale=np.std(X_scaled, axis=0), size=(30, X_scaled.shape[1]))
+synth_data = np.random.normal(loc=np.mean(_test_scaled, axis=0), scale=np.std(X_test_scaled, axis=0), size=(30, X_test_scaled.shape[1]))
 synth_scaled = scaler.transform(synth_data)
 synth_pred = knn.predict(synth_scaled)
 print(f'Accuracy of synthetic data prediction: {accuracy_score(y_test, synth_pred)}')
